1bf398cba1693b9beb87e620456b48b4282ddd67
Ojo Oluwaseun Joseph <smarzzy5@gmail.com>
2020-03-07 19:11:53 +0100

    Authentication APi with Node and Express

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const port = process.env.PORT
+const userRouter = require('./routers/user')
+require('./db/db')
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
